5c23cacad7d4d465890f935f684c76d08d1dd49d
Sibi77 <zavadskijdmitrij7@gmail.com>
2023-03-10 09:00:21 +0300

    page thanks

---
+++ thanks.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Template name: thanks
+ */
+get_header(); ?>
+    <div class="pir-container">
+        <div class="pir-breadcrumbs" style="padding-top: 100px">
+            <?php true_breadcrumbs();?>
+        </div>
+    </div>
+<section class="thanks">
+    <div class="pir-container">
+        <div class="thanks__wrap">
+            <h1>Спасибо</h1>
+            <p>за проявленный интерес</p>
+            <img src="<?php bloginfo('template_directory') ;?>/img/icons/phone-la.svg" alt="phone">
+            <p>Наш специалист перезвонит вам в течении 10 минут</p>
+            <a href="#" class="pir-btn">Назад</a>
+        </div>
+    </div>
+</section>
+<?php get_footer(); ?>
