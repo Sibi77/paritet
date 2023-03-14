ba3f80f1e17b8815e59c623bbb958ad806488463
Sibi77 <zavadskijdmitrij7@gmail.com>
2023-03-14 10:31:37 +0300

    search

--- search.php
+++ search.php
@@ -1,26 +1,26 @@
 <?php
-///**
-// * The template for displaying search results pages
-// *
-// * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
-// *
-// */
-//get_header();
-//if ( have_posts() && strlen( trim(get_search_query()) ) != 0 ) :?>
-<!---->
-<!--                <header class="page-header">-->
-<!--                    <h1 class="page-title">--><?php
-//                        /* translators: %s: search query. */
-//                        printf( esc_html__( 'Search Results for: %s', '_s' ), '<span>' . get_search_query() . '</span>' );
-//                        ?><!--</h1>-->
-<!--                </header><!-- .page-header -->-->
-<!---->
-<!--                --><?php
-//                /* Start the Loop */
-//                while ( have_posts() ) : the_post();
-//                    get_template_part( 'template-parts/content', 'search' );
-//                endwhile;
-//            endif; ?>
-<?php //get_footer();
+/**
+ * The template for displaying search results pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
+ *
+ */
+get_header();
+if ( have_posts() && strlen( trim(get_search_query()) ) != 0 ) :?>
+
+                <header class="page-header">
+                    <h1 class="page-title"><?php
+                        /* translators: %s: search query. */
+                        printf( esc_html__( 'Search Results for: %s', '_s' ), '<span>' . get_search_query() . '</span>' );
+                        ?></h1>
+                </header><!-- .page-header -->
+
+                <?php
+                /* Start the Loop */
+                while ( have_posts() ) : the_post();
+                    get_template_part( 'template-parts/content', 'search' );
+                endwhile;
+            endif; ?>
+<?php get_footer();
 
 
