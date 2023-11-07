047c18d433673c502ddf8f7c6db56c0cb9283cd4
Sibi77 <zavadskijdmitrij7@gmail.com>
2023-11-07 09:08:33 +0300

    merge1

---
+++ framework-customizations/extensions/shortcodes/shortcodes/button/views/view.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined( 'FW' ) ) {
+    die( 'Forbidden' );
+}
+/**
+ * @var int $form_id
+ * @var string $submit_button_text
+ * @var array $extra_data
+ */
+?>
+
+<a href="<?php echo $atts['btn_link'] ?>" class="pir-btn"><?php echo $atts['btn_text'] ?></a>
